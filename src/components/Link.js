e6220f04d6cd52d9e7c0bf8728570290de5847ea
Morgane Dubus <morgane.d@meilisearch.com>
2021-12-20 17:00:58 +0100

    Add Link component

---
+++ src/components/Link.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import NextLink from 'next/link'
+import get from 'utils/get'
+
+const StyledLink = styled.a`
+  color: inherit;
+  transition: color 300ms;
+  &:hover,
+  &:focus {
+    color: ${get('colors.hotPink')};
+  }
+`
+
+const Link = React.forwardRef(({ href, ...props }, ref) => (
+  <NextLink href={href} passHref>
+    <StyledLink ref={ref} {...props} />
+  </NextLink>
+))
+
+Link.displayName = 'Link'
+
+Link.propTypes = {}
+
+Link.defaultProps = {}
+
+export default Link
