d22231b609bc9e0519afda258ee94db334302e57
Morgane Dubus <morgane.d@meilisearch.com>
2021-12-23 09:54:48 +0100

    Add icons to storybook

---
+++ src/stories/Icons.stories.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {
+  ApiReference as ApiReferenceIcon,
+  Arrow as ArrowIcon,
+  Github as GithubIcon,
+  GithubOutlined as GithubOutlinedIcon,
+  Guides as GuidesIcon,
+  Hamburger as HamburgerIcon,
+  Integrations as IntegrationsIcon,
+  Roadmap as RoadmapIcon,
+  Star as StarIcon,
+} from 'components/icons'
+
+export default {
+  title: 'Components/Icons',
+  parameters: {
+    layout: 'padded',
+  },
+  decorators: [
+    Story => (
+      <div
+        style={{
+          color: 'white',
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+export const ApiReference = () => <ApiReferenceIcon height={56} />
+export const Arrow = () => <ArrowIcon height={56} />
+export const Github = () => <GithubIcon height={56} />
+export const GithubOutlined = () => <GithubOutlinedIcon height={56} />
+export const Guides = () => <GuidesIcon height={56} />
+export const Hamburger = () => <HamburgerIcon height={56} />
+export const Integrations = () => <IntegrationsIcon height={56} />
+export const Roadmap = () => <RoadmapIcon height={56} />
+export const Star = () => <StarIcon height={56} />
