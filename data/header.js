f416ff3af58e358ae6c74025c84cc30541a5cda2
qdequele <quentin@dequelen.me>
2022-01-25 12:10:08 +0100

    update header links

--- data/header.js
+++ data/header.js
@@ -9,18 +9,18 @@ import {
 const getHeaderData = (t = () => {}) => ({
   links: [
     {
-      title: t('documentation'),
-      href: 'https://docs.meilisearch.com/',
+      title: t('documentation.title'),
+      href: t('documentation.url'),
       target: '_blank',
     },
     {
-      title: t('blog'),
-      href: 'https://blog.meilisearch.com/',
+      title: t('blog.title'),
+      href: t('blog.url'),
       target: '_blank',
     },
     {
-      title: t('jobs'),
-      href: 'https://www.welcometothejungle.com/fr/companies/meilisearch',
+      title: t('jobs.title'),
+      href: t('jobs.url'),
       target: '_blank',
     },
     {
@@ -30,47 +30,47 @@ const getHeaderData = (t = () => {}) => ({
           title: t('resources.subMenu.0.title'),
           subtitle: t('resources.subMenu.0.subtitle'),
           icon: Guides,
-          href: 'https://docs.meilisearch.com/learn/',
+          href: t('resources.subMenu.0.url'),
           target: '_blank',
         },
         {
           title: t('resources.subMenu.1.title'),
           subtitle: t('resources.subMenu.1.subtitle'),
           icon: ApiReference,
-          href: 'https://docs.meilisearch.com/reference/api/',
+          href: t('resources.subMenu.1.url'),
           target: '_blank',
         },
         {
           title: t('resources.subMenu.2.title'),
           subtitle: t('resources.subMenu.2.subtitle'),
           icon: Roadmap,
-          href: 'https://roadmap.meilisearch.com/tabs/5-ideas',
+          href: t('resources.subMenu.2.url'),
           target: '_blank',
         },
         {
           title: t('resources.subMenu.3.title'),
           subtitle: t('resources.subMenu.3.subtitle'),
           icon: GithubOutlined,
-          href: 'https://github.com/meilisearch/meilisearch/releases',
+          href: t('resources.subMenu.3.url'),
           target: '_blank',
         },
         {
           title: t('resources.subMenu.4.title'),
           subtitle: t('resources.subMenu.4.subtitle'),
           icon: Integrations,
-          href: 'https://github.com/meilisearch/integration-guides',
+          href: t('resources.subMenu.4.url'),
           target: '_blank',
         },
       ],
     },
   ],
   cta1: {
-    title: t('logIn'),
-    href: '/',
+    title: t('logIn.title'),
+    href: t('logIn.url'),
   },
   cta2: {
-    title: t('getStarted'),
-    href: '/',
+    title: t('getStarted.title'),
+    href: t('getStarted.url'),
   },
 })
 
