61546dac91138b4e764bf54200494f5d2d21a773
qdequele <quentin@meilisearch.com>
2022-01-27 22:03:25 +0100

    add crisp script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from 'theme/GlobalStyle'
+import Script from 'next/script'
 import theme from 'theme'
 import { appWithTranslation } from 'next-i18next'
 
@@ -8,6 +9,13 @@ export function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Script
+        id="crisp-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="38a71dd2-729c-4970-b061-3e1db1c1eb20";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`,
+        }}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   )
