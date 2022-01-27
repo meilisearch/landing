7485443f76139ab66d7cc4e7fce37a48a3754b65
qdequele <quentin@meilisearch.com>
2022-01-27 22:04:00 +0100

    change fathom place

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -16,6 +16,12 @@ export function App({ Component, pageProps }) {
           __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="38a71dd2-729c-4970-b061-3e1db1c1eb20";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`,
         }}
       />
+      <Script
+        id="fathom-script"
+        strategy="afterInteractive"
+        src="https://cdn.usefathom.com/script.js"
+        data-site="QNBPJXIV"
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   )
