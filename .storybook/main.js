95e1a0b562457044996e4f62def6408f1936beb5
Morgane Dubus <morgane.d@meilisearch.com>
2022-04-05 16:14:46 +0200

    Update static dir settings for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     '../src/stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  staticDirs: ['../public'],
   webpackFinal: async (config, { configType }) => {
     config.resolve.alias = {
       ...config.resolve.alias,
