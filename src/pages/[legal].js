eea3159550bbc9b28d064a1b66fee1af365e8840
Morgane Dubus <morgane.d@meilisearch.com>
2022-04-27 17:37:37 +0200

    Add menu links

--- src/pages/[legal].js
+++ src/pages/[legal].js
@@ -6,6 +6,8 @@ import glob from 'glob'
 import Typography from 'components/Typography'
 import get from 'utils/get'
 import PreTitle from 'components/PreTitle'
+import Link from 'components/Link'
+import { useRouter } from 'next/router'
 
 const Markdown = styled.div`
   color: ${get('colors.ashes.900')};
@@ -28,36 +30,77 @@ const Markdown = styled.div`
   }
 `
 
-const LegalPage = ({ markdownBody }) => (
-  <>
-    <PreTitle preTitle="Legal" color={get('colors.hotPink')} />
-    <Markdown>
-      <ReactMarkdown
-        components={{
-          h1: props => (
-            <Typography variant="title.m" forwardedAs="h1" {...props} />
-          ),
-          h2: props => (
-            <Typography variant="title.s" forwardedAs="h2" {...props} />
-          ),
-          h3: props => (
-            <Typography variant="title.xs" forwardedAs="h3" {...props} />
-          ),
-        }}
-      >
-        {markdownBody}
-      </ReactMarkdown>
-    </Markdown>
-  </>
-)
+const LegalLinks = styled.div`
+  color: ${get('colors.ashes.900')};
+  margin: 80px 0;
+  .active {
+    color: ${get('colors.hotPink')};
+  }
+`
+
+const LegalPage = ({ markdownBody, legalPages }) => {
+  const router = useRouter()
+  console.log(router)
+  return (
+    <>
+      <LegalLinks>
+        {legalPages.map(legalPage => (
+          <Link
+            href={`/${legalPage}`}
+            key={legalPage}
+            className={router.asPath == `/${legalPage}` ? 'active' : ''}
+          >
+            <Typography style={{ display: 'block' }}>
+              {legalPage.replace(new RegExp('-', 'g'), ' ')}
+            </Typography>
+          </Link>
+        ))}
+      </LegalLinks>
+      <div>
+        <PreTitle preTitle="Legal" color={get('colors.hotPink')} />
+        <Markdown>
+          <ReactMarkdown
+            components={{
+              h1: props => (
+                <Typography variant="title.m" forwardedAs="h1" {...props} />
+              ),
+              h2: props => (
+                <Typography variant="title.s" forwardedAs="h2" {...props} />
+              ),
+              h3: props => (
+                <Typography variant="title.xs" forwardedAs="h3" {...props} />
+              ),
+            }}
+          >
+            {markdownBody}
+          </ReactMarkdown>
+        </Markdown>
+      </div>
+    </>
+  )
+}
+
+const getLegalPages = () => {
+  const markdownPages = glob.sync('src/pages/**/*.mdx')
+
+  const slugs = markdownPages.map(file => {
+    const parts = file.split('/')
+    return parts[parts.length - 1].replace('.mdx', '')
+  })
+
+  return slugs
+}
 
 export async function getStaticProps({ locale, params }) {
   const { legal } = params
   const content = await import(`./${legal}.mdx`)
   const data = matter(content.default)
 
+  const legalPages = getLegalPages()
+
   return {
     props: {
+      legalPages,
       ...(await serverSideTranslations(locale, ['header', 'footer'])),
       markdownBody: data.content,
     },
@@ -65,14 +108,9 @@ export async function getStaticProps({ locale, params }) {
 }
 
 export async function getStaticPaths() {
-  const blogs = glob.sync('src/pages/**/*.mdx')
+  const slugs = getLegalPages()
 
-  const blogSlugs = blogs.map(file => {
-    const parts = file.split('/')
-    return parts[parts.length - 1].replace('.mdx', '')
-  })
-
-  const paths = blogSlugs.map(slug => ({
+  const paths = slugs.map(slug => ({
     params: { legal: slug },
   }))
 
