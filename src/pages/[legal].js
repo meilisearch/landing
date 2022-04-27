1b1608b63c1fc91e52eade0e69fc4b91978accdb
Morgane Dubus <morgane.d@meilisearch.com>
2022-04-27 15:34:11 +0200

    Update markdown style

--- src/pages/[legal].js
+++ src/pages/[legal].js
@@ -1,10 +1,54 @@
+import styled from 'styled-components'
 import matter from 'gray-matter'
 import ReactMarkdown from 'react-markdown'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import glob from 'glob'
+import Typography from 'components/Typography'
+import get from 'utils/get'
+import PreTitle from 'components/PreTitle'
+
+const Markdown = styled.div`
+  color: ${get('colors.ashes.900')};
+  font-family: Inter;
+  h1 {
+    color: ${get('colors.white')};
+    display: block;
+  }
+  h2 {
+    color: ${get('colors.ashes.600')};
+    display: block;
+  }
+  h3 {
+    color: ${get('colors.hotPink')};
+    display: block;
+  }
+  a {
+    color: ${get('colors.white')};
+    border-bottom: 1px solid ${get('colors.hotPink')};
+  }
+`
 
 const LegalPage = ({ markdownBody }) => (
-  <ReactMarkdown>{markdownBody}</ReactMarkdown>
+  <>
+    <PreTitle preTitle="Legal" color={get('colors.hotPink')} />
+    <Markdown>
+      <ReactMarkdown
+        components={{
+          h1: props => (
+            <Typography variant="title.m" forwardedAs="h1" {...props} />
+          ),
+          h2: props => (
+            <Typography variant="title.s" forwardedAs="h2" {...props} />
+          ),
+          h3: props => (
+            <Typography variant="title.xs" forwardedAs="h3" {...props} />
+          ),
+        }}
+      >
+        {markdownBody}
+      </ReactMarkdown>
+    </Markdown>
+  </>
 )
 
 export async function getStaticProps({ locale, params }) {
