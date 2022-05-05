29bdc1d02a31552b247ef532047656ed63691319
Morgane Dubus <morgane.d@meilisearch.com>
2022-05-05 10:11:44 +0200

    Add scroll to bottom librayry to capture lazy loading images

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -1,4 +1,5 @@
 const WAITING_TIME = 5000
+let scrollToBottom = require('scroll-to-bottomjs')
 
 describe(`Homepage`, () => {
   it('Should visit the Home page', () => {
@@ -10,6 +11,7 @@ describe(`Homepage`, () => {
   it('Should match snapshot', () => {
     cy.visit('/')
     cy.wait(WAITING_TIME)
+    cy.window().then(cyWindow => scrollToBottom({ remoteWindow: cyWindow }))
     cy.percySnapshot('home-hero-responsive', { widths: [768, 1024, 1440] })
   })
 })
