3bec3d32ec30ecc6a8b65b4cabb7649a27a8f98c
Morgane Dubus <morgane.d@meilisearch.com>
2022-05-18 14:47:11 +0200

    Update 404 page

---
+++ data/404.js
@@ -0,0 +1,12 @@
+const get404Data = () => ({
+  meta: {
+    title: '',
+    description: '',
+  },
+  content: {
+    description: 'Sorry, the page you are looking for does not exist.',
+    cta: 'Bring me home, please',
+  },
+})
+
+export default get404Data
