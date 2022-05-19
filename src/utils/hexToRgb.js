609c7edc11083a34518f6c21311de15538f54a47
Morgane Dubus <morgane.d@meilisearch.com>
2022-05-19 13:17:02 +0200

    Add hexToRgb function

---
+++ src/utils/hexToRgb.js
@@ -0,0 +1,9 @@
+const hexToRgb = (hex, alpha) => {
+  const r = parseInt(hex.slice(1, 3), 16)
+  const g = parseInt(hex.slice(3, 5), 16)
+  const b = parseInt(hex.slice(5, 7), 16)
+
+  return `rgba(${r}, ${g}, ${b}${alpha ? `, ${alpha}` : ''})`
+}
+
+export default hexToRgb
