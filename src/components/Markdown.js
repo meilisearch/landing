1e22995653b51e9d86cf943848f5e7385a169850
Morgane Dubus <morgane.d@meilisearch.com>
2022-05-19 13:16:49 +0200

    Update markdown styles

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -8,6 +8,7 @@ const StyledMarkdown = styled.div`
   color: ${get('colors.ashes.900')};
   margin-top: 16px;
   font-family: Inter;
+  word-break: break-word;
   h1 {
     color: ${get('colors.white')};
     display: block;
@@ -15,14 +16,19 @@ const StyledMarkdown = styled.div`
   h2 {
     color: ${get('colors.ashes.600')};
     display: block;
+    margin: 32px 0 24px;
   }
   h3 {
     color: ${get('colors.hotPink')};
     display: block;
+    margin: 24px 0 16px;
   }
   a {
     color: ${get('colors.white')};
     border-bottom: 1px solid ${get('colors.hotPink')};
+    @media (min-width: ${get('breakpoints.md')}) {
+      font-size: ;
+    }
   }
 `
 
