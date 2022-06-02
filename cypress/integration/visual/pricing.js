e7a03e0e5595415897c97560a18e8ed893b35388
Morgane Dubus <morgane.d@meilisearch.com>
2022-06-02 14:06:42 +0200

    fix pricing page url

--- cypress/integration/visual/pricing.js
+++ cypress/integration/visual/pricing.js
@@ -3,7 +3,7 @@ let scrollToBottom = require('scroll-to-bottomjs')
 
 describe(`Pricing`, () => {
   it('Should take a snapshot of the pricing page', () => {
-    cy.visit('/')
+    cy.visit('/pricing')
     cy.wait(WAITING_TIME)
     // Scroll the entire page to load lazy images
     cy.window().then(cyWindow =>
