c4d64e0401f433b53b2f31a8d30026f992720282
Morgane Dubus <morgane.d@meilisearch.com>
2022-06-06 16:48:49 +0200

    Remove analytics and tracking scripts in not in production mode

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,36 +4,47 @@ import Script from 'next/script'
 import theme from 'theme'
 import BaseLayout from 'layouts/BaseLayout'
 
+const Scripts = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return (
+      <>
+        <Script
+          id="crisp-script"
+          dangerouslySetInnerHTML={{
+            __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="38a71dd2-729c-4970-b061-3e1db1c1eb20";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`,
+          }}
+        />
+        <Script
+          id="fathom-script"
+          src="https://cdn.usefathom.com/script.js"
+          data-site="QNBPJXIV"
+        />
+        <Script
+          id="hotjar-script"
+          dangerouslySetInnerHTML={{
+            __html: `
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:1035453,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+          }}
+        />
+      </>
+    )
+  }
+  return null
+}
+
 // Template for every page
 export function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Script
-        id="crisp-script"
-        dangerouslySetInnerHTML={{
-          __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="38a71dd2-729c-4970-b061-3e1db1c1eb20";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`,
-        }}
-      />
-      <Script
-        id="fathom-script"
-        src="https://cdn.usefathom.com/script.js"
-        data-site="QNBPJXIV"
-      />
-      <Script
-        id="hotjar-script"
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:1035453,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-        }}
-      />
+      <Scripts />
       <BaseLayout>
         <Component {...pageProps} />
       </BaseLayout>
