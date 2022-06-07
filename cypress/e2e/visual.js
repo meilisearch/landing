049c3f8db02408807f774560635abbae6e179f82
Morgane Dubus <morgane.d@meilisearch.com>
2022-06-07 12:17:22 +0200

    Add waiting time before taking a screenshot with percy

--- cypress/e2e/visual.js
+++ cypress/e2e/visual.js
@@ -1,3 +1,4 @@
+const WAITING_TIME = 1000
 let scrollToBottom = require('scroll-to-bottomjs')
 
 describe(`Visual tests`, () => {
@@ -7,6 +8,7 @@ describe(`Visual tests`, () => {
     cy.window().then(cyWindow =>
       scrollToBottom({ remoteWindow: cyWindow, frequency: 80, timing: 10 })
     )
+    cy.wait(WAITING_TIME)
     cy.get("img[alt='Developer']").should('be.visible')
     cy.get('.meilisearch-logo-footer').should('be.visible')
     cy.get('.meilisearch-logo-header').should('be.visible')
@@ -17,6 +19,7 @@ describe(`Visual tests`, () => {
     cy.visit('/pricing')
     // Scroll the entire page to load lazy images
     cy.window().then(cyWindow => scrollToBottom({ remoteWindow: cyWindow }))
+    cy.wait(WAITING_TIME)
     cy.get('.meilisearch-logo-header').should('be.visible')
     cy.percySnapshot('pricing-responsive', { widths: [375, 1024, 1440] })
   })
@@ -25,6 +28,7 @@ describe(`Visual tests`, () => {
     cy.visit('/privacy-policy')
     // Scroll the entire page to load lazy images
     cy.window().then(cyWindow => scrollToBottom({ remoteWindow: cyWindow }))
+    cy.wait(WAITING_TIME)
     cy.get('.meilisearch-logo-header').should('be.visible')
     cy.percySnapshot('privacy-policy')
   })
@@ -33,12 +37,14 @@ describe(`Visual tests`, () => {
     cy.visit('/terms-of-use')
     // Scroll the entire page to load lazy images
     cy.window().then(cyWindow => scrollToBottom({ remoteWindow: cyWindow }))
+    cy.wait(WAITING_TIME)
     cy.get('.meilisearch-logo-header').should('be.visible')
     cy.percySnapshot('terms-of-use')
   })
 
   it('Should take a snapshot of the 404 page', () => {
     cy.visit('/test', { failOnStatusCode: false })
+    cy.wait(WAITING_TIME)
     cy.get('.meilisearch-logo-header').should('be.visible')
     cy.percySnapshot('404')
   })
