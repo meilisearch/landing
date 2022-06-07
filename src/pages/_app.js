91bd9f7108a105b00f8a872bd566dd31206f6b4f
Morgane Dubus <morgane.d@meilisearch.com>
2022-06-07 11:06:30 +0200

    Remove scripts analytics when running on ci

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,24 +5,29 @@ import theme from 'theme'
 import BaseLayout from 'layouts/BaseLayout'
 
 const Scripts = () => {
-  if (process.env.NODE_ENV === 'production') {
-    return (
-      <>
-        <Script
-          id="crisp-script"
-          dangerouslySetInnerHTML={{
-            __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="38a71dd2-729c-4970-b061-3e1db1c1eb20";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`,
-          }}
-        />
-        <Script
-          id="fathom-script"
-          src="https://cdn.usefathom.com/script.js"
-          data-site="QNBPJXIV"
-        />
-        <Script
-          id="hotjar-script"
-          dangerouslySetInnerHTML={{
-            __html: `
+  if (
+    process.env.NODE_ENV !== 'production' ||
+    process.env.NEXT_PUBLIC_ENVIRONMENT === 'test'
+  ) {
+    return null
+  }
+  return (
+    <>
+      <Script
+        id="crisp-script"
+        dangerouslySetInnerHTML={{
+          __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="38a71dd2-729c-4970-b061-3e1db1c1eb20";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`,
+        }}
+      />
+      <Script
+        id="fathom-script"
+        src="https://cdn.usefathom.com/script.js"
+        data-site="QNBPJXIV"
+      />
+      <Script
+        id="hotjar-script"
+        dangerouslySetInnerHTML={{
+          __html: `
         (function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
             h._hjSettings={hjid:1035453,hjsv:6};
@@ -31,12 +36,10 @@ const Scripts = () => {
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
             a.appendChild(r);
         })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-          }}
-        />
-      </>
-    )
-  }
-  return null
+        }}
+      />
+    </>
+  )
 }
 
 // Template for every page
