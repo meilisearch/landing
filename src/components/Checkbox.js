575e67912d333076cbf1036e5e71fb7059b03358
Morgane Dubus <morgane.d@meilisearch.com>
2022-06-22 10:26:32 +0200

    Add color white on selected elements

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -15,21 +15,25 @@ const HiddenCheckBox = styled(ReakitCheckbox)`
 
 const VisibleCheckBox = styled(IconCard)`
   cursor: pointer;
-  &:hover,
-  &:focus-within {
-    border: 1px solid ${get('colors.white')};
-  }
   border: 1px solid ${get('colors.valhalla.100')};
   border-radius: 8px;
-  transition: border 300ms, background-color 300ms;
+  transition: border 300ms, background-color 300ms, color 300ms;
 
+  &:hover,
+  &:focus-within,
   ${HiddenCheckBox}:focus + & {
     border: 1px solid ${get('colors.white')};
+    color: ${get('colors.white')};
   }
   ${HiddenCheckBox}[aria-checked="true"] + & {
     border: 1px solid ${p => get(`colors.${getColorName(p.$color)}`)};
+    color: ${get('colors.white')};
     background-color: ${p =>
       hexToRgb(get(`colors.${getColorName(p.$color)}`), 0.2)};
+  }
+  ${HiddenCheckBox}[aria-checked="true"]:hover + &,
+  ${HiddenCheckBox}[aria-checked="true"]:focus + & {
+    border: 1px solid ${get('colors.white')};
   }
 `
 
--- src/components/Radio.js
+++ src/components/Radio.js
@@ -16,21 +16,25 @@ const HiddenRadio = styled(ReakitRadio)`
 
 const CardRadio = styled.div`
   cursor: pointer;
-  &:hover,
-  &:focus-within {
-    border: 1px solid ${get('colors.white')};
-  }
   border: 1px solid ${get('colors.valhalla.100')};
   border-radius: 8px;
-  transition: border 300ms, background-color 300ms;
+  transition: border 300ms, background-color 300ms, color 300ms;
 
+  &:hover,
+  &:focus-within,
   ${HiddenRadio}:focus + & {
     border: 1px solid ${get('colors.white')};
+    color: ${get('colors.white')};
   }
   ${HiddenRadio}[aria-checked="true"] + & {
     border: 1px solid ${p => get(`colors.${getColorName(p.$color)}`)};
+    color: ${get('colors.white')};
     background-color: ${p =>
       hexToRgb(get(`colors.${getColorName(p.$color)}`), 0.2)};
+  }
+  ${HiddenRadio}[aria-checked="true"]:hover + &,
+  ${HiddenRadio}[aria-checked="true"]:focus + & {
+    border: 1px solid ${get('colors.white')};
   }
 `
 
