653572b1c5d745f13cf1588f9eb0f86b6ef82f83
Morgane Dubus <morgane.d@meilisearch.com>
2022-08-02 15:22:41 +0200

    Fix next config additional property

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   poweredByHeader: false,
-  webpack5: true,
   webpack: config => {
     // Unset client-side javascript that only works server-side
     config.resolve.fallback = { fs: false, module: false }
