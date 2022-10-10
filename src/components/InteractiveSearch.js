3960c7be627acaab0acee2e9ecd57965fa3015ae
Ariful Alam <swazan.arif@gmail.com>
2022-10-10 22:41:48 +0600

    Declare the search config as variables

--- src/components/InteractiveSearch.js
+++ src/components/InteractiveSearch.js
@@ -9,6 +9,14 @@ import Typography from 'components/Typography'
 import Lottie from 'components/Lottie'
 import { useInView } from 'react-intersection-observer'
 
+const NEXT_PUBLIC_DEMO_MOVIES_HOST = process.env.NEXT_PUBLIC_DEMO_MOVIES_HOST
+  ? process.env.NEXT_PUBLIC_DEMO_MOVIES_HOST
+  : 'http://0.0.0.0:7700'
+const NEXT_PUBLIC_DEMO_MOVIES_API_KEY = process.env
+  .NEXT_PUBLIC_DEMO_MOVIES_API_KEY
+  ? process.env.NEXT_PUBLIC_DEMO_MOVIES_API_KEY
+  : 'searchKey'
+
 const Card = styled.div`
   background-color: ${get('colors.white')};
   border-radius: 12px;
@@ -123,8 +131,8 @@ const InteractiveSearch = ({
   const searchClient = React.useMemo(
     () =>
       instantMeiliSearch(
-        process.env.NEXT_PUBLIC_DEMO_MOVIES_HOST ? process.env.NEXT_PUBLIC_DEMO_MOVIES_HOST: 'http://0.0.0.0:7700',
-        process.env.NEXT_PUBLIC_DEMO_MOVIES_API_KEY ? process.env.NEXT_PUBLIC_DEMO_MOVIES_API_KEY: 'searchKey',
+        NEXT_PUBLIC_DEMO_MOVIES_HOST,
+        NEXT_PUBLIC_DEMO_MOVIES_API_KEY,
         {
           paginationTotalHits: 4,
           primaryKey: 'id',
