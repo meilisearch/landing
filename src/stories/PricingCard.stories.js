fb6d0d2321d6ac2d319ba8d904852359ca0158b2
Morgane Dubus <morgane.d@meilisearch.com>
2022-10-18 11:46:36 +0200

    Update pricing card component

--- src/stories/PricingCard.stories.js
+++ src/stories/PricingCard.stories.js
@@ -1,51 +1,31 @@
 import React from 'react'
 import PricingCard from 'components/PricingCard'
-import Typography from 'components/Typography'
-import get from 'utils/get'
-import { darken } from 'utils/colors'
+import getPricingPageData from '../../data/pricing'
 
 export default {
   title: 'Components/PricingCard',
   component: PricingCard,
   parameters: {
     layout: 'padded',
+    backgrounds: { default: 'white' },
   },
+  decorators: [
+    Story => (
+      <div
+        style={{
+          width: 250,
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
 }
 
 export const Default = () => {
-  return (
-    <PricingCard>
-      <PricingCard.Header>
-        <Typography variant="title.m">Open Source</Typography>
-      </PricingCard.Header>
-      <PricingCard.Body>
-        <Typography>
-          I love cheese, especially everyone loves smelly cheese. Port-salut
-          taleggio cheese on toast blue castello st. agur blue cheese paneer
-          roquefort bocconcini. Parmesan ricotta queso cheese and biscuits
-          emmental cheesecake fondue hard cheese. Paneer queso macaroni cheese
-          rubber cheese smelly cheese emmental babybel bocconcini. Edam.
-        </Typography>
-      </PricingCard.Body>
-    </PricingCard>
-  )
-}
+  const {
+    pricing: { payingPlans },
+  } = getPricingPageData()
 
-export const OtherColor = () => {
-  return (
-    <PricingCard>
-      <PricingCard.Header $color={darken(get('colors.dodgerBlue'), 60)}>
-        <Typography variant="title.m">Open Source</Typography>
-      </PricingCard.Header>
-      <PricingCard.Body>
-        <Typography>
-          I love cheese, especially everyone loves smelly cheese. Port-salut
-          taleggio cheese on toast blue castello st. agur blue cheese paneer
-          roquefort bocconcini. Parmesan ricotta queso cheese and biscuits
-          emmental cheesecake fondue hard cheese. Paneer queso macaroni cheese
-          rubber cheese smelly cheese emmental babybel bocconcini. Edam.
-        </Typography>
-      </PricingCard.Body>
-    </PricingCard>
-  )
+  return <PricingCard plan={payingPlans[0]} />
 }
