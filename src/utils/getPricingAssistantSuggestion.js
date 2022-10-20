da08fbb7b2d07d4069ef4a925837b0da06a6cd89
Morgane Dubus <morgane.d@meilisearch.com>
2022-10-20 10:39:32 +0200

    Update pricing assistant plans

--- src/utils/getPricingAssistantSuggestion.js
+++ src/utils/getPricingAssistantSuggestion.js
@@ -1,39 +1,25 @@
 export const SUGGESTIONS = {
   CUSTOM: 'CUSTOM',
-  RAM1_CPU05_DISK10: {
-    monthlyPrice: 29,
+  HOBBY: {
+    monthlyPrice: 30,
     hourPrice: 0.04,
     ram: 1,
     cpu: 0.5,
     disk: 10,
   },
-  RAM4_CPU1_DISK40: {
-    monthlyPrice: 122,
+  DEVELOPER: {
+    monthlyPrice: 120,
     hourPrice: 0.17,
     ram: 4,
     cpu: 1,
     disk: 40,
   },
-  RAM8_CPU2_DISK80: {
-    monthlyPrice: 257,
-    hourPrice: 0.357,
-    ram: 8,
-    cpu: 2,
-    disk: 80,
-  },
-  RAM16_CPU4_DISK160: {
-    monthlyPrice: 461,
-    hourPrice: 0.64,
+  PRO: {
+    monthlyPrice: 600,
+    hourPrice: 0.82,
     ram: 16,
     cpu: 4,
     disk: 160,
-  },
-  RAM32_CPU8_DISK320: {
-    monthlyPrice: 972,
-    hourPrice: 1.35,
-    ram: 32,
-    cpu: 8,
-    disk: 320,
   },
 }
 
@@ -66,10 +52,9 @@ const getPricingAssistantSuggestion = form => {
     dbSizeInGB += (documentsNumber * documentApproxSizeInKB * 2) / 1024 / 1024
   }
 
-  if (dbSizeInGB <= 10) return SUGGESTIONS.RAM1_CPU05_DISK10
-  if (dbSizeInGB <= 40) return SUGGESTIONS.RAM4_CPU1_DISK40
-  if (dbSizeInGB <= 80) return SUGGESTIONS.RAM8_CPU2_DISK80
-  if (dbSizeInGB <= 160) return SUGGESTIONS.RAM16_CPU4_DISK160
+  if (dbSizeInGB <= 10) return SUGGESTIONS.HOBBY
+  if (dbSizeInGB <= 40) return SUGGESTIONS.DEVELOPER
+  if (dbSizeInGB <= 160) return SUGGESTIONS.PRO
   if (dbSizeInGB > 160) return SUGGESTIONS.CUSTOM
 }
 
