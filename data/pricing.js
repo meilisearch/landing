52c90e98da6cf2b9f3f801d8a5ce73cd884832d0
Morgane Dubus <morgane.d@meilisearch.com>
2022-10-24 15:52:13 +0200

    Update links and faq

--- data/pricing.js
+++ data/pricing.js
@@ -43,7 +43,7 @@ const pricingPageData = {
         ],
         cta: {
           title: 'Get started',
-          href: '', // TODO: ask for the link
+          href: 'https://cloud.meilisearch.com/register?plan=hobby',
           target: '_blank',
         },
       },
@@ -61,7 +61,7 @@ const pricingPageData = {
         ],
         cta: {
           title: 'Get started',
-          href: '', // TODO: ask for the link
+          href: 'https://cloud.meilisearch.com/register?plan=developer',
           target: '_blank',
         },
       },
@@ -79,7 +79,7 @@ const pricingPageData = {
         ],
         cta: {
           title: 'Get started',
-          href: '', // TODO: ask for the link
+          href: 'https://cloud.meilisearch.com/register?plan=pro',
           target: '_blank',
         },
       },
@@ -98,7 +98,7 @@ const pricingPageData = {
         ],
         cta: {
           title: 'Contact Sales',
-          href: '', // TODO: ask for the link
+          href: 'mailto:bonjour@meilisearch.com?subject="Enterprise quote"',
           target: '_blank',
         },
       },
@@ -110,10 +110,10 @@ const pricingPageData = {
       description:
         'Start using Meilisearch now by downloading our open source version. You will have the possibility in the future to move to our cloud easily.',
       readMore:
-        'Read our <a href="" target="_blank">Cloud - Open source comparison guide</a>', // TODO: ask for the link
+        'Read our <a href="#open-source-vs-cloud">Cloud - Open source comparison guide</a>',
       cta: {
         title: 'Get started',
-        href: '', // TODO: ask for the link
+        href: 'https://docs.meilisearch.com/learn/getting_started/quick_start.html',
         target: '_blank',
       },
     },
@@ -479,35 +479,40 @@ const pricingPageData = {
     title: 'FAQs',
     questions: [
       {
-        question: 'What is the best server size for my needs?',
+        question: 'Is there a free plan or a trial period?',
         answer:
-          'The size of the machine you need will depend on many factors. The number of documents, the size of those documents, the number of filters/sorts you will need. If you want to get a quick estimate of how big you can get to start with, get a machine that has at least ten times the disk space of your dataset.',
+          'We currently do not offer a free plan or trial period. However, you can try out the open-source version following this <a href="https://docs.meilisearch.com/learn/getting_started/quick_start.html" target="_blank">quick start guide</a>.',
       },
       {
-        question: 'Is Meilisearch cloud highly available?',
+        question: 'Is there a minimum term or commitment once I start paying?',
         answer:
-          'Yes, no need to sell you 3 nodes, Meillisearch is highly available by default. It’s totally transparent for you.',
+          'There is no minimum term, and users can close their accounts anytime. Users will only be charged for the period during which a project was active. That said, if you’re unhappy with Meilisearch, we’d like to know why. It’s important to us that our users are happy. Please do not hesitate to contact us through the chat widget or email us at <a href="mailto:cloud@meilisearch.com">cloud@meilisearch.com</a> and provide a detailed explanation of what happened. We’ll work with you to ensure you have the best possible experience. To delete your account, navigate to the account settings on your dashboard and click the “Delete your account” button.',
       },
       {
-        question:
-          'I have never used a search engine before. Can I use Meilisearch anyway?',
+        question: 'How do we accept payment?',
         answer:
-          "Of course! No knowledge of ElasticSearch or Solr is required to use Meilisearch. Meilisearch is really easy to use and thus accessible to all kinds of developers. Take a quick tour to learn the basics of Meilisearch! We also provide a lot of tools, including SDKs, to help you integrate easily Meilisearch in your project. We're adding new tools every day! Plus, you can contact us if you need any help. We will answer for sure!",
+          'We only accept payment online via credit cards and charge in US dollars (USD) with Stripe as our payment service provider.',
       },
       {
-        question: 'Do I need to configure Meilisearch to get it working?',
+        question: 'How does billing work?',
         answer:
-          "Meilisearch configuration works out-of-the-box. It means, by default, Meilisearch configures necessary settings for providing a powerful and relevant search. For example, without requiring any configuration, Meilisearch is typo tolerant. Type \"create an index\" in the documentation search bar to experience the typo tolerance of our search engine. To find out more about the relevancy of Meilisearch, take a look at <a href='https://docs.meilisearch.com/learn/core_concepts/relevancy.html' target='_blank'>this detailed explanation</a>. However, Meilisearch is of course highly customizable in order to adapt the search to your needs by setting synonyms, stop words, and custom ranking rules.",
+          'We bill per hour spent. We do not provide consumption-based plans. An hour corresponds to one hour of a Meilisearch server or project up and running in the cloud. In addition, while computing the hours, we round up, meaning if a server runs for one hour and five minutes, we will charge for two hours; if it runs for thirty minutes, we will charge for one hour.<br/><br/>Users will be charged at the end of each month based on their invoice(s) value. You can view the estimated cost for a billing period of your project(s) on the <a href="https://cloud.meilisearch.com/billing" target="_blank">billing page</a> and access your invoices and receipts by navigating to the “Billing settings” > “Manage billing settings and invoices” > “Invoice History”.',
       },
       {
-        question: 'How to know if Meilisearch perfectly fits my use cases?',
+        question: 'Can I change my plan or resource?',
         answer:
-          "Since Meilisearch is an open-source and easy-to-use tool, you can give it a try using your data. Follow <a href='https://docs.meilisearch.com/learn/getting_started/quick_start.html' target='_blank'>this guide</a> to get a quick start! Besides, we published <a href='https://docs.meilisearch.com/learn/what_is_meilisearch/comparison_to_alternatives.html' target='_blank'>a comparison between Meilisearch and other search engines</a> with the goal of providing an overview of Meilisearch alternatives.",
+          "Yes, and it is effortless. You can upgrade a project's plan or resource by selecting the project, navigating to the “Resources” tab, and clicking on the “Upgrade” button. Please remember that once the process is complete, you won't be able to downgrade your resource.",
       },
       {
-        question: 'Which languages can Meilisearch handle?',
-        answer:
-          'Meilisearch works perfectly with English, kanji, and Romance languages. If you have any hesitation about your language handling, please contact us.',
+        question: 'How do I choose the right plan?',
+        answer: `You can estimate your costs and the right plan for your use case using our <a href="#pricing-assistant">pricing assistant</a>. We often suggest choosing a plan that corresponds with the number of documents, as upgrading to a higher plan with more capacity is very easy. While we take into account the number of documents in estimating the plan to choose, here are some other factors to consider when choosing a plan:<br/>
+          <ul>
+            <li><strong>Volume or number of search requests:</strong> A search request is CPU intensive. The more cores you provide to the engine, the more search queries it will be able to process at the same time. Kindly note that using 0.5 vCPUs might impact the search experience while indexing simultaneously. Hence, we recommend using at least one full CPU to handle indexing and search concurrently, meaning Meilisearch will automatically take 0.5 vCPUs maximum for indexing, with the other for search.</li>
+            <li><strong>Search speed:</strong> The search speed is based on the RAM/database size ratio. You can find more information in the <a href="https://docs.meilisearch.com/faq.html#search-speed" target="_blank">dedicated documentation</a>.</li>
+            <li><strong>Disk space:</strong> We recommend reserving at least ten times the size of your raw data. This means that if your document size is 8GB, you should at least reserve 80GB of disk space for Meilisearch.</li>
+            <li><strong>Complexity and structure of your documents:</strong> If there are lots of filterable and searchable attributes or all document fields are searchable, we strongly recommend updating the \`searchableAttributes\` list to contain only the fields you want to search; otherwise, a plan with more RAM and storage would be suitable.</li>
+          </ul><br/>
+          If you need help choosing the right plan for your use case, please do not hesitate to contact us through the chat widget or email us at <a href="mailto:cloud@meilisearch.com">cloud@meilisearch.com</a>.`,
       },
     ],
   },
