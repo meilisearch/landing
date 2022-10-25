7fba4bd99eb51767642e6d7cce98801ef5925c52
Morgane Dubus <morgane.d@meilisearch.com>
2022-10-25 12:02:42 +0200

    Update about us data

--- data/about-us.js
+++ data/about-us.js
@@ -1,4 +1,4 @@
-const getAboutUsData = () => ({
+const aboutUsData = {
   meta: {
     title: 'About us',
     description: '',
@@ -7,6 +7,6 @@ const getAboutUsData = () => ({
     title:
       '<strong>Meilisearch</strong> is a french startup 🇫🇷 created in 2018 🎂 that just sealed a $15M series A round 💸, and has a 25+ worldwide-based team 🌍 committed to shaping the best search experience 🔍 ever made 🎉.',
   },
-})
+}
 
-export default getAboutUsData
+export default aboutUsData
