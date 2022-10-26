44608089b3466370b419a37e8d0dbe8f589be942
Morgane Dubus <morgane.d@meilisearch.com>
2022-10-26 16:28:28 +0200

    Create the our story block

--- data/about-us.js
+++ data/about-us.js
@@ -7,6 +7,11 @@ const aboutUsData = {
     title:
       '<strong>Meilisearch</strong> is a french startup 🇫🇷 created in 2018 🎂 that just sealed a $15M series A round 💸, and has a 25+ worldwide-based team 🌍 committed to shaping the best search experience 🔍 ever made 🎉.',
   },
+  ourStory: {
+    preTitle: 'meilisearch Milestones',
+    title: 'Our story',
+    image: '/images/timeline.png',
+  },
 }
 
 export default aboutUsData
