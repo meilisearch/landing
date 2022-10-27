657447f4bab927f81b3a5b4b36ebe72ac0ce538d
Morgane Dubus <morgane.d@meilisearch.com>
2022-10-27 16:28:09 +0200

    Add investors block

--- data/about-us.js
+++ data/about-us.js
@@ -1,3 +1,13 @@
+import {
+  Crv,
+  Felicis,
+  KimaVentures,
+  LocalGlobe,
+  Mango,
+  Seedcamp,
+  Vercel,
+} from 'components/icons'
+
 const aboutUsData = {
   meta: {
     title: 'About us',
@@ -11,6 +21,48 @@ const aboutUsData = {
     preTitle: 'meilisearch Milestones',
     title: 'Our story',
     image: '/images/timeline.png',
+  },
+  investors: {
+    preTitle: 'Lorem Ipsum', // TODO : Update with text
+    title: 'Our investors',
+    description: 'We are proudly backed by some of the world’s best investors.',
+    list: [
+      {
+        icon: Felicis,
+        names: ['Viviana Faga', 'Tobi Coker '],
+        company: 'Felicis',
+      },
+      {
+        icon: Crv,
+        names: ['Reid Christian', 'Brittany Walker'],
+        company: 'CRV',
+      },
+      {
+        icon: LocalGlobe,
+        names: ['Mish Mashkaustan'],
+        company: 'LocalGlobe & Latitude',
+      },
+      {
+        icon: Vercel,
+        names: ['Guillermo Rauch'],
+        company: 'Vercel',
+      },
+      {
+        icon: Mango,
+        names: ['Robin Vasan'],
+        company: 'Mango Capital',
+      },
+      {
+        icon: Seedcamp,
+        names: ['Sia Houchangnia'],
+        company: 'Seedcamp',
+      },
+      {
+        icon: KimaVentures,
+        names: ['Alexis Robert'],
+        company: 'Kima Ventures',
+      },
+    ],
   },
 }
 
