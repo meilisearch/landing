3310098fd371934ed9779a0db7e0f8097827219e
Morgane Dubus <morgane.d@meilisearch.com>
2022-11-03 12:09:56 +0100

    Apply suggestions from code review

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -8,12 +8,11 @@ const TagWrapper = styled.div`
   color: ${get('colors.ashes')};
   border: 1px solid ${p => get(p.$color)};
   border-radius: 8px;
-  padding: 5px;
+  padding: 4px 7px;
   display: inline-flex;
   align-items: center;
   justify-content: center;
   background-color: ${p => hexToRgb(get(p.$color), 0.2)};
-  height: 30px;
   min-width: 70px;
 `
 
