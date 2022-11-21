b1a0e7d72064b5e924c2858de10b36395051e027
vasucp1207 <vasucp1207@gmail.com>
2022-11-21 16:12:02 +0000

    fixing hamburger bug on safari

--- src/components/Dialog/DialogDisclosure.js
+++ src/components/Dialog/DialogDisclosure.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import { DialogDisclosure as ReakitDialogDisclosure } from 'reakit/Dialog'
 
 const Disclosure = styled.div`
+  max-width: 32px;
   background-color: transparent;
   border: none;
   cursor: pointer;
