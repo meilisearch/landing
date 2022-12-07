0e9d85556817e032505f93bfa1960e75f43823d3
Morgane Dubus <30866152+mdubus@users.noreply.github.com>
2022-12-07 12:24:01 +0100

    Update src/utils/shuffle.js
    Co-authored-by: cvermand <33010418+bidoubiwa@users.noreply.github.com>

--- src/utils/shuffle.js
+++ src/utils/shuffle.js
@@ -1,8 +1,8 @@
 const shuffle = array => {
   let shuffleArray = [...array]
   for (let i = shuffleArray.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1))
-    let temp = shuffleArray[i]
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = shuffleArray[i]
     shuffleArray[i] = shuffleArray[j]
     shuffleArray[j] = temp
   }
